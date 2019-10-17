2dec7ac04e2f53e50f020e221f8cf4fa59e3c69c
lukia zhao <yifengzhao0530@gmail.com>
2019-10-17 22:26:19 +1100

    [init]: init project with frontend (vue 3.0) and backend api (.net core 2.1)

---
+++ zed-vue-3.0/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{result}}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      result: 'string'
+    }
+  },
+  methods: {
+    test () {
+      console.log('test api')
+      axios.get('/api/values').then(response => {
+        console.log(response)
+      })
+    }
+  },
+  mounted () {
+    this.test()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ zed-vue-3.0/src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+    <b-navbar-brand href="#"><img src="../assets/company_logo.png" id="company_logo" alt="Kitten">
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Study</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+#company_logo {
+  height:50px;
+}
+.nav-item {
+  font-size: 20px;
+  font-weight: bold;
+}
+.navbar-light .navbar-nav .nav-link {
+    color: #4BA7DB;
+}
+</style>
