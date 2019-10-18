2906e9441f903fa92eda7ec7c9714c89466bf33f
lukia zhao <yifengzhao0530@gmail.com>
2019-10-18 16:57:25 +1100

    [backend]: add study model, IStudyManager, StudyManager, api endpoints

--- Zed-dotnet/Zed-dotnet/Program.cs
+++ Zed-dotnet/Zed-dotnet/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Zeddotnet.Models;
 
 namespace Zed_dotnet
 {
@@ -15,6 +16,7 @@ namespace Zed_dotnet
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            //StudyManager studyManager = new StudyManager();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
---
+++ Zed-dotnet/Zed-dotnet/Services/StudyManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Zeddotnet.Interfaces;
+using Zeddotnet.Models;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Zeddotnet.Services
+{
+    public class StudyManager : IStudyManager
+    {
+        private Dictionary<string, Study> studies;
+
+        public StudyManager()
+        {
+            if (studies == null || studies.Count == 0)
+            {
+                studies = LoadStudies();
+            }
+
+        }
+
+        public Dictionary<string, Study> LoadStudies()
+        {
+            string root = "./Files";
+            string[] fileNames = Directory.GetFiles(root);
+
+            Dictionary<string, Study> tempStudies = new Dictionary<string, Study>();
+
+            foreach (string fileName in fileNames)
+            {
+                var jsonText = File.ReadAllText(fileName);
+                Study study = JsonConvert.DeserializeObject<Study>(jsonText);
+                tempStudies.Add(study.GUID, study);
+            }
+
+            return tempStudies;
+        }
+
+        public Dictionary<string, Study> GetAll()
+        {
+            return studies;
+        }
+
+        public Study GetStudyByGUID(string guid)
+        {
+            return studies[guid];
+        }
+    }
+}
